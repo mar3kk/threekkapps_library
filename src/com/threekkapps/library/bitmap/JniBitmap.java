f3db9cca7a5574974aa8c70aeae958ebb2f23b67
mar3kk <mar3kk@gmail.com>
2014-01-21 21:55:47 +0100

    added javadoc to flooFill method

--- src/com/threekkapps/library/bitmap/JniBitmap.java
+++ src/com/threekkapps/library/bitmap/JniBitmap.java
@@ -10,6 +10,13 @@ public class JniBitmap {
 	static {
 		System.loadLibrary("jnibitmap");
 	}
-
+	/**
+	 * Flood fill algorithm. Fills with specified color starting from specified
+	 * x,y coordinates. 
+	 * @param bitmap Bitmap to apply the flood fill algorith
+	 * @param x x coordinate where the algorithm starts
+	 * @param y y coordinate where the algorithm starts
+	 * @param color Color of fill in ARGB format
+	 **/
 	public static native void floodFill(Bitmap bitmap, int x, int y, int color);
 }
