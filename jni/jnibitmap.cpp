81e4af76d8163dc17e1151895ec60d849cbac1e2
Euginius <eugene84.p@gmail.com>
2014-12-06 15:22:55 +0200

    Added support for tolerance
    Added support to set a tolerance value ti the start color to be
    colored. With can be useful to solve the problem of not coloring the
    border of the shape due to minor color/alpha changes of the initial
    color.

--- jni/jnibitmap.cpp
+++ jni/jnibitmap.cpp
@@ -16,13 +16,14 @@
 #define DEBUG 0
 
 extern "C" {
-  JNIEXPORT void JNICALL Java_com_threekkapps_library_bitmap_JniBitmap_floodFill(JNIEnv * env, jobject obj, jobject handle, uint32_t x, uint32_t y, uint32_t color);
-  void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, AndroidBitmapInfo* bitmapInfo);
+  JNIEXPORT void JNICALL Java_com_threekkapps_library_bitmap_JniBitmap_floodFill(JNIEnv * env, jobject obj, jobject handle, uint32_t x, uint32_t y, uint32_t color, uint32_t tolerance);
+  bool isPixelValid(int currentColor,int oldColor,int *startColor,int tolerance);
+  void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, AndroidBitmapInfo* bitmapInfo, uint32_t tolerance);
 }
 
 
 
-JNIEXPORT void JNICALL Java_com_threekkapps_library_bitmap_JniBitmap_floodFill(JNIEnv * env, jobject obj, jobject bitmap, uint32_t x, uint32_t y, uint32_t color) {
+JNIEXPORT void JNICALL Java_com_threekkapps_library_bitmap_JniBitmap_floodFill(JNIEnv * env, jobject obj, jobject bitmap, uint32_t x, uint32_t y, uint32_t color, uint32_t tolerance) {
 	AndroidBitmapInfo bitmapInfo;
 	uint32_t* storedBitmapPixels = NULL;
 
@@ -44,13 +45,40 @@ JNIEXPORT void JNICALL Java_com_threekkapps_library_bitmap_JniBitmap_floodFill(J
 
 	}
 
-	floodFill(x,y,color, bitmapPixels, &bitmapInfo);
+	floodFill(x,y,color, bitmapPixels, &bitmapInfo,tolerance);
 
 	AndroidBitmap_unlockPixels(env, bitmap);
 
 }
 
-void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, AndroidBitmapInfo* bitmapInfo) {
+    bool isPixelValid(int currentColor,int oldColor,int *startColor,int tolerance) {
+
+    	if(tolerance != 0) {
+    		int alpha = ((currentColor & 0xFF000000) >> 24);
+    		int red = ((currentColor & 0xFF0000) >> 16) * alpha / 255; // red
+    		int green = ((currentColor & 0x00FF00) >> 8) * alpha / 255; // Green
+    		int blue = (currentColor & 0x0000FF) * alpha / 255; // Blue
+
+    				return (red >= (startColor[0] - tolerance)
+    						&& red <= (startColor[0] + tolerance)
+    						&& green >= (startColor[1] - tolerance)
+    						&& green <= (startColor[1] + tolerance)
+    						&& blue >= (startColor[2] - tolerance)
+    						&& blue <= (startColor[2] + tolerance));
+    	}else {
+    		if(currentColor == oldColor){
+				return true;
+			}else {
+				return false;
+			}
+    	}
+	}
+
+void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, AndroidBitmapInfo* bitmapInfo,uint32_t tolerance) {
+
+		// Used to hold the the start( touched ) color that we like to change/fill
+		int values [3] = { };
+
 
     	if (x >bitmapInfo->width-1)
     		return;
@@ -68,6 +96,13 @@ void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, Andro
     	int red = 0; int blue = 0; int green = 0; int alpha = 0;
     	oldColor = pixels[y*bitmapInfo->width+x];
 
+    	// Get red,green and blue values of the old color we like to chnage
+    	alpha = (int) ((color & 0xFF000000) >> 24);
+    	values[0] = (int) ((oldColor & 0xFF0000) >> 16) * alpha / 255; // red
+    	values[1] = (int)((oldColor & 0x00FF00) >> 8) * alpha / 255; // Green
+    	values[2] = (int) (oldColor & 0x0000FF) * alpha / 255; // Blue
+
+
     	alpha = (int) ((color & 0xFF000000) >> 24);
     	blue = (int) ((color & 0xFF0000) >> 16);
 		green = (int)((color & 0x00FF00) >> 8);
@@ -75,6 +110,7 @@ void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, Andro
 		blue = blue * alpha / 255;
 		green = green * alpha / 255;
 		red = red * alpha / 255;
+
 		int tmp = 0;
 		tmp = red;
 		red = blue;
@@ -83,6 +119,8 @@ void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, Andro
 		color =  ((alpha<< 24) & 0xFF000000) | ((blue<< 16) & 0xFF0000) |
 		              ((green << 8) & 0x00FF00) |
 		              (red & 0x0000FF);
+
+
 		LOGD("AndroidBitmap_lockPixels() failed ! error=%d");
 		std::queue < uint32_t > pixelsX;
 		std::queue < uint32_t > pixelsY;
@@ -92,7 +130,6 @@ void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, Andro
 
     	pixelsX.push(x);
     	pixelsY.push(y);
-
 
     	while (!pixelsX.empty()) {
 
@@ -106,7 +143,7 @@ void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, Andro
     		wx = nx; wy = ny;
     		ex = wx+1; ey = wy;
 
-    		while (wx>0 && pixels[wy*bitmapInfo->width+wx] == oldColor) {
+    		while (wx>0 && isPixelValid(pixels[wy*bitmapInfo->width+wx],oldColor,values,tolerance)) {
     			pixels[wy*bitmapInfo->width+wx] = color;
 
     			if (wy > 0 && pixels[(wy-1)*bitmapInfo->width+wx] == oldColor) {
@@ -114,7 +151,7 @@ void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, Andro
     				pixelsY.push(wy-1);
 
     			}
-    			if (wy < bitmapInfo->height - 1  && pixels[(wy+1)*bitmapInfo->width+wx] == oldColor) {
+    			if (wy < bitmapInfo->height - 1  && isPixelValid(pixels[(wy+1)*bitmapInfo->width+wx],oldColor,values,tolerance)) {
     				pixelsX.push(wx);
     				pixelsY.push(wy+1);
 
@@ -123,14 +160,14 @@ void floodFill(uint32_t x, uint32_t y, uint32_t color, void* bitmapPixels, Andro
     		}
 
 
-    		while (ex < bitmapInfo->width -1 && pixels[ey*bitmapInfo->width+ex] == oldColor) {
+    		while (ex < bitmapInfo->width -1 && isPixelValid(pixels[ey*bitmapInfo->width+ex],oldColor,values,tolerance)) {
 				pixels[ey*bitmapInfo->width+ex] = color;
-				if (ey > 0 && pixels[(ey-1)*bitmapInfo->width+ex] == oldColor) {
+				if (ey > 0 && isPixelValid(pixels[(ey-1)*bitmapInfo->width+ex],oldColor,values,tolerance)) {
 					pixelsX.push(ex);
 					pixelsY.push(ey-1);
 
 				}
-				if (ey < bitmapInfo->height - 1  && pixels[(ey+1)*bitmapInfo->width+ex] == oldColor) {
+				if (ey < bitmapInfo->height - 1  && isPixelValid(pixels[(ey+1)*bitmapInfo->width+ex],oldColor,values,tolerance)) {
 					pixelsX.push(ex);
 					pixelsY.push(ey+1);
 
